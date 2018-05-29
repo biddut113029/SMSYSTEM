a9f90035bceeb1e906e8153a3b2f47916bb66107
biddut.oct2017@gmail.com <biddut.oct2017@gmail.com>
2018-05-29 17:33:10 +0600

    deleted1

--- ASPCoreWithAngular/ClientApp/app/components/app/app.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'AppComponent',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css']
-})
-export class AppComponent {
-}
